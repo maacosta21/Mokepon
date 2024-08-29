8a2885c1bd79075ebea96aab29c7e79621778a83
Mike DEV <vacacionesvillavicencio@gmail.com>
2024-08-28 19:04:03 -0500

    Mokepon v2

--- MokeponJS.js
+++ MokeponJS.js
@@ -7,28 +7,35 @@ function aleatorio(max, min) {
     return Math.floor(Math.random() * (max - min + 1)+min)
 }
 
-function vidasCounter() {
-    while(jugadorCounter < 3 && pcCounter < 3) {
-        
-        let jugador = prompt('Elige un numero')
+function eleccion(sujeto, nombre){
 
-    if(jugador == 1){
-        alert('elegiste piedra')
-    } else if(jugador == 2) {
-        alert('elegiste papel')
-    } else if(jugador == 3){alert('elegiste tijera')}
+    if(sujeto == 1){
+        sujeto = 'piedra'
+        alert(`${nombre} ha elegido ${sujeto}`)
+    } else if(sujeto == 2) {
+        sujeto = 'papel'
+        alert(`${nombre} ha elegido ${sujeto}`)
+    } else if(sujeto == 3){
+        sujeto = 'tijera'
+        alert(`${nombre} ha elegido ${sujeto}`)}
     else{
         alert('elige un numero')
         let jugador = prompt('Elige un numero')
     }
-     
-    let pc = aleatorio(3,1)
+
+}
+
+function vidasCounter() {
+    while(jugadorCounter < 3 && pcCounter < 3) {
+
+        let jugador = prompt('Elige un numero')
+        let pc = aleatorio(3,1)
+
+
+        eleccion(jugador,'Jugador')
+        eleccion(pc,'PC')
     
-    if(pc == 1){
-        alert('PC elige piedra')
-    } else if(pc == 2) {
-        alert('PC elige papel')
-    } else if(pc == 3){alert('PC elige tijera')}
+
     
     if (jugador == 1 && pc == 3) {
         alert('Ganaste')
@@ -37,7 +44,7 @@ function vidasCounter() {
         alert('Ganaste')
         jugadorCounter ++
     
-    }else if(jugador == 3 && pc == 2){
+    }else if(jugador == 3 && pc == 2 ){
         alert('Ganaste')
         jugadorCounter ++
     }else if (jugador == pc){
