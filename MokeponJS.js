98c39eec1c3f478e377cec9a1d50f71f40606532
Mike DEV <vacacionesvillavicencio@gmail.com>
2024-08-31 15:20:44 -0500

    v3

--- MokeponJS.js
+++ MokeponJS.js
@@ -1,61 +1,52 @@
+let jugadorMascota
+let pcMascota
 let jugadorCounter = 0
 let pcCounter = 0
 const max = 3
 const min = 1
 
+//Variables DOM
+
+const botonMacostaJuagador = document.querySelector('#boton-mascota')
+const mascotaHipodoge = document.querySelector('#Hipodoge')
+const mascotaCapipepo = document.querySelector('#Capipepo')
+const mascotaRatigueya = document.querySelector('#Ratigueya')
+const mascotaJugadorDom = document.querySelector('#PlayerPET')
+const mascotaPcDom = document.querySelector('#PCPET')
+botonMacostaJuagador.addEventListener('click', alertas)
+
+function alertas() {
+    if(mascotaHipodoge.checked) {
+        jugadorMascota = 'Hipodoge'
+        mascotaPCSelect()
+    } else if(mascotaCapipepo.checked) {
+        jugadorMascota = 'Capipepo'
+        mascotaPCSelect()
+    } else if(mascotaRatigueya.checked) {
+        jugadorMascota = 'Ratigueya'
+        mascotaPCSelect()
+    } else {alert('Elige una mascosta')}
+    
+}
+
 function aleatorio(max, min) {
-    return Math.floor(Math.random() * (max - min + 1)+min)
+     return Math.floor(Math.random() * (max - min + 1)+min) 
 }
 
-function eleccion(sujeto, nombre){
+function mascotaPCSelect() {
+    let pcAleatorio = aleatorio(3,1)
 
-    if(sujeto == 1){
-        sujeto = 'piedra'
-        alert(`${nombre} ha elegido ${sujeto}`)
-    } else if(sujeto == 2) {
-        sujeto = 'papel'
-        alert(`${nombre} ha elegido ${sujeto}`)
-    } else if(sujeto == 3){
-        sujeto = 'tijera'
-        alert(`${nombre} ha elegido ${sujeto}`)}
-    else{
-        alert('elige un numero')
-        let jugador = prompt('Elige un numero')
-    }
-
-}
-
-function vidasCounter() {
-    while(jugadorCounter < 3 && pcCounter < 3) {
-
-        let jugador = prompt('Elige un numero')
-        let pc = aleatorio(3,1)
-
-
-        eleccion(jugador,'Jugador')
-        eleccion(pc,'PC')
-    
-
-    
-    if (jugador == 1 && pc == 3) {
-        alert('Ganaste')
-        jugadorCounter ++
-    } else if(jugador == 2 && pc == 1){
-        alert('Ganaste')
-        jugadorCounter ++
-    
-    }else if(jugador == 3 && pc == 2 ){
-        alert('Ganaste')
-        jugadorCounter ++
-    }else if (jugador == pc){
-        alert('empate')
+    if(pcAleatorio == 1){
+        pcMascota = 'Hipodoge'
+    } else if(pcAleatorio == 2) {
+        pcMascota = 'Capipepo'
     } else {
-        alert('perdiste') 
-        pcCounter ++
+        pcMascota = 'Ratigueya'
     }
-    }
-} vidasCounter()
 
+    mascotaJugadorDom.innerHTML = jugadorMascota
+    mascotaPcDom.innerHTML = pcMascota
+}
 
 
 
